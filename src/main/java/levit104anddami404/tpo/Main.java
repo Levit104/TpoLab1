1ccff1e6974ae718ab7526d4559ae88e9dec455a
Artyom <artyom0104@gmail.com>
2024-04-02 12:06:54 +0300

    Add Lab1

--- src/main/java/levit104anddami404/tpo/Main.java
+++
@@ -1,7 +0,0 @@
-package levit104anddami404.tpo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/levit104anddami404/tpo/lab1/task3/Door.java
@@ -0,0 +1,14 @@
+package levit104anddami404.tpo.lab1.task3;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Door {
+    private final boolean isFit;
+    private boolean isLocked;
+}
